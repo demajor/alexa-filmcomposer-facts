64f0639239c390048a7bc789e8c167fa98c6465e
Doug Major <demajor@demajorMBP.local>
2017-07-06 17:56:31 -0700

    film composer fact skill v1: completed

--- src/index.js
+++ src/index.js
@@ -1,42 +1,32 @@
 'use strict';
 var Alexa = require('alexa-sdk');
 
-//=========================================================================================================================================
-//TODO: The items below this comment need your attention.
-//=========================================================================================================================================
+var APP_ID = "amzn1.ask.skill.a2e4fdaa-4f55-4508-a7a6-00dcd5444f31";
 
-//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.  
-//Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
-var APP_ID = undefined;
-
-var SKILL_NAME = "Space Facts";
+var SKILL_NAME = "Film Composer Facts";
 var GET_FACT_MESSAGE = "Here's your fact: ";
-var HELP_MESSAGE = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
+var HELP_MESSAGE = "You can say tell me a film composer fact, or, you can say exit... What can I help you with?";
 var HELP_REPROMPT = "What can I help you with?";
 var STOP_MESSAGE = "Goodbye!";
 
-//=========================================================================================================================================
-//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
-//=========================================================================================================================================
 var data = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
+    "Danny Elfman's first major film score gig was for Tim Burton's 1985 film Pee-wee's Big Adventure.",
+    "Philip Glass received an Academy Award nomination for his scoring work on the film, The Hours.",
+    "Fences, the Denzel Washington directed and highly acclaimed film, featured a score by NY based composer Marcelo Zarvos.",
+    "The English producer and composer, Jon Hopkins, was nomintated for an Ivor Novello Award for Best Original Score on the 2010 film, Monsters.",
+    "Philip Glass has noted that his favorite composer is Franz Schubert.",
+    "Danny Elfman composed the theme song for The Simpsons in 1989.",
+    "Clint Mansell composed the scores for director Darren Aronofsky's, now cult film hits, Pi and Requiem for a Dream.",
+    "Jeff Beal won Television Composer of the Year, mainly for his scores on the Netflix hit show House of Cards.",
+    "In 2015, composer Alexandre Desplat won an Academy Award for his original score in the Wes Anderson film The Grand Budapest Hotel.",
+    "Composer Angelo Badalamenti won a Grammy Award for Best Pop Instrumental Performance in 1990, for writing the Twin Peaks Theme.",
+    "Composer Hans Zimmer suggests that when composing a theme for film or TV, that you treat the melodic stucture of the theme as set of questions and answers.",
+    "According to composer Cliff Martinez, one of the most important aspects of writing music for film is to truly understand the directors' unique vision for the film.",
+    "Elmer Bernstein composed the themes and other musical pieces for over 200 films and TV shows over his fifty year career.",
+    "Composer Clint Mansell won the 2007 Public Choice Award, from the World Soundtrack Academy, for his exquisite work on the film, The Fountain.",
+    "All original score segments, within the 2014 film Birdman, were composed and performed on solo jazz percussion by Antonio Sánchez."
 ];
 
-//=========================================================================================================================================
-//Editing anything below this line might break your skill.  
-//=========================================================================================================================================
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
